b195ac1ec3ac621386b370f06aecdf210559255f
Melissa Deven <99757260+Deven1991@users.noreply.github.com>
2022-05-30 16:53:26 -0500

    updated readme and portfolio section

--- README.md
+++ README.md
@@ -4,33 +4,37 @@
 
 Advanced CSS Portfolio webpage includes a general biography for Melissa Deven along with work portfolio expamples for future employers. Additionally, the webpage includes various method to contact Melissa Deven.
 
+My portfolio of work showcases my skills and talents to future employers. 
+
 
 ## Table of Contents (Optional)
 
-If your README is long, add a table of contents to make it easy for users to find what they need.
-
-- [About Me](#about me)
-- [Portfolio](#portfolio)
-- [Contact](#contact)
+- [User Story](#userstory)
+- [Webpage](#webpage)
+- [Usage](#usage)
 - [Links](#links)
 
+## UserStory
+```
+AS AN employer
+I WANT to view a potential employee's deployed portfolio of work samples
+SO THAT I can review samples of their work and assess whether they're a good candidate for an open position
+```
 
-## About Me
+## Webpage
 
-The first section include Melissa Deven's biography including education and work experience.
+The first section include Melissa Deven's biography including education and work experience. You can use the navigation pane at the top right corner of the webpage to navigage to various sections, including specific artowrk webpages. 
+
+Additionally, the below video showcases how to use the webpage and its functionality. 
+
+![alt text](assets/images/recording.gif)
+
+## Usage
+
+The webpage was primarily built with HTML and CSS. As alterate CSS frameworks, Bootstrap and Bulma were both used to enhance the webpaeg and ease of use.
 
 
-## Portfolio
-
-The section section includes three examples of applications with links. 
-
-    ```md
-    ![alt text](assets/images/screenshot.png)
-    ```
-
-## Contact
-
-This section includes various methods to contact myself - including phone, e-mail, location, and linked LinkedIn page.
+![alt text](assets/images/recording.gif)
 
 
 ## Links
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,44 @@
-// Access toggle switch HTML element
-var themeSwitcher = document.querySelector("#theme-switcher");
-var container = document.querySelector(".container");
-
-// Set default mode to dark
-var mode = "dark";
-
-// Listen for a click event on toggle switch
-themeSwitcher.addEventListener("click", function () {
-    // If mode is dark, apply light background
-    if (mode === "dark") {
-        mode = "light";
-        container.setAttribute("class", "light");
+document.addEventListener('DOMContentLoaded', () => {
+    // Functions to open and close a modal
+    function openModal($el) {
+        $el.classList.add('is-active');
     }
-    // If mode is light, apply dark background 
-    else {
-        mode = "dark";
-        container.setAttribute("class", "dark");
+
+    function closeModal($el) {
+        $el.classList.remove('is-active');
     }
+
+    function closeAllModals() {
+        (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+            closeModal($modal);
+        });
+    }
+
+    // Add a click event on buttons to open a specific modal
+    (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+        const modal = $trigger.dataset.target;
+        const $target = document.getElementById(modal);
+
+        $trigger.addEventListener('click', () => {
+            openModal($target);
+        });
+    });
+
+    // Add a click event on various child elements to close the parent modal
+    (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+        const $target = $close.closest('.modal');
+
+        $close.addEventListener('click', () => {
+            closeModal($target);
+        });
+    });
+
+    // Add a keyboard event to close all modals
+    document.addEventListener('keydown', (event) => {
+        const e = event || window.event;
+
+        if (e.keyCode === 27) { // Escape key
+            closeAllModals();
+        }
+    });
 });
